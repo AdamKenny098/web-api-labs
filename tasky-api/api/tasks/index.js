0130119627ce2052c95f558a5b8383cf876dc2c5
AdamKenny098 <adamtkenny@gmail.com>
2024-12-20 00:59:20 +0000

    Mongo lab Included Mongoose

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import Task from './taskModel';
-import asyncHandler from 'express-async-handler';
 
 const router = express.Router(); // eslint-disable-line
 
@@ -11,10 +10,10 @@ router.get('/', async (req, res) => {
 });
 
 // create a task
-router.post('/', asyncHandler(async (req, res) => {
+router.post('/', async (req, res) => {
     const task = await Task(req.body).save();
     res.status(201).json(task);
-}));
+});
 
 // Update Task
 router.put('/:id', async (req, res) => {
@@ -23,7 +22,7 @@ router.put('/:id', async (req, res) => {
         _id: req.params.id,
     }, req.body);
     if (result.matchedCount) {
-        res.status(200).json({ code:200, msg: 'Task Updated Sucessfully' });
+        res.status(200).json({ code:200, msg: 'Task Updated Successfully' });
     } else {
         res.status(404).json({ code: 404, msg: 'Unable to find Task' });
     }
@@ -41,4 +40,6 @@ router.delete('/:id', async (req, res) => {
         res.status(404).json({ code: 404, msg: 'Unable to find Task' });
     }
 });
+
+
 export default router;
